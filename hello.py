29587f12dabc0a91f0ecd26c2dea26f0a0feae19
SirBenedick <beneweb@outlook.de>
2018-04-07 14:27:28 +0200

    Bot

--- hello.py
+++ hello.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
 # def hello_example():
 #     return example
 
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+# @app.route('/hello/<name>')
+# def hello_name(name):
+#    return 'Hello %s!' % name
