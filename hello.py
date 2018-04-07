04c8f1d88fb103aa819ab8bbeee4920f83600b47
SirBenedick <beneweb@outlook.de>
2018-04-07 14:25:27 +0200

    D

--- hello.py
+++ hello.py
@@ -1,29 +1,26 @@
 from flask import Flask
 import os
-
-# import telepot
-# import datetime
-#example = environ.get('EXAMPLE)
-
+import telepot
+import datetime
+BOT_TOKEN = os.environ.get('BOT_TOKEN',3)
 app = Flask(__name__)
 
-# def handle(msg):
-#         chat_id = msg['chat']['id']
-#         command = msg['text']
-#         user = str(chat_id)
-#         msg = ""
-#         print( 'User: ' + user + ' Nachricht: ' + str(command) )
-#
-#         elif command == '/time':
-#                 bot.sendMessage(chat_id, str(datetime.datetime.now()))
-#
-# bot = telepot.Bot('43211234')
-# bot.message_loop(handle)
+def handle(msg):
+        chat_id = msg['chat']['id']
+        command = msg['text']
+        user = str(chat_id)
+        msg = ""
+        print( 'User: ' + user + ' Nachricht: ' + str(command) )
+
+        elif command == '/time':
+                bot.sendMessage(chat_id, str(datetime.datetime.now()))
+
+bot = telepot.Bot(BOT_TOKEN)
+bot.message_loop(handle)
 
 @app.route('/')
 def hello_world():
-    times = os.environ.get('BOT_TOKEN',3)
-    return times
+    return "hello"
 
 if __name__ == "__main__":
 	app.run()
@@ -33,6 +30,6 @@ if __name__ == "__main__":
 # def hello_example():
 #     return example
 
-# @app.route('/hello/<name>')
-# def hello_name(name):
-#    return 'Hello %s!' % name
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
