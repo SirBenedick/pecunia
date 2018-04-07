d33360ae2a7e94efe10fe437834ed2c50e39ed61
SirBenedick <beneweb@outlook.de>
2018-04-07 13:21:35 +0200

    dasasd

--- hello.py
+++ hello.py
@@ -1,30 +1,30 @@
 from flask import Flask
-import telepot
-import datetime
-example = environ.get('EXAMPLE)
+# import telepot
+# import datetime
+#example = environ.get('EXAMPLE)
 
 app = Flask(__name__)
 
-def handle(msg):
-        chat_id = msg['chat']['id']
-        command = msg['text']
-        user = str(chat_id)
-        msg = ""
-        print( 'User: ' + user + ' Nachricht: ' + str(command) )
-
-        elif command == '/time':
-                bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
-#bot = telepot.Bot('43211234')
-#bot.message_loop(handle)
+# def handle(msg):
+#         chat_id = msg['chat']['id']
+#         command = msg['text']
+#         user = str(chat_id)
+#         msg = ""
+#         print( 'User: ' + user + ' Nachricht: ' + str(command) )
+#
+#         elif command == '/time':
+#                 bot.sendMessage(chat_id, str(datetime.datetime.now()))
+#
+# bot = telepot.Bot('43211234')
+# bot.message_loop(handle)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/env')
-def hello_world():
-    return example
+# @app.route('/env')
+# def hello_world():
+#     return example
 
 @app.route('/hello/<name>')
 def hello_name(name):
