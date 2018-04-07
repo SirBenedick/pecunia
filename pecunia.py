5083199eac694d323bbaeac9505669befb2a8204
SirBenedick <beneweb@outlook.de>
2018-04-07 15:10:04 +0200

    pure test

--- pecunia.py
+++ pecunia.py
@@ -1,23 +1,24 @@
 #1.0
-import telepot
-import datetime
+# import telepot
+# import datetime
+#
+# BOT_TOKEN = str(os.environ.get('BOT_TOKEN',3))
+#
+# def handle(msg):
+#         chat_id = msg['chat']['id']
+#         command = msg['text']
+#         user = str(chat_id)
+#         msg = ""
+#         print( 'User: ' + user + ' Nachricht: ' + str(command) )
+#
+#         elif command == '/time':
+#                 bot.sendMessage(chat_id, str(datetime.datetime.now()))
+#
+# bot = telepot.Bot(BOT_TOKEN)
+# bot.message_loop(handle)
+#
+# print( 'I am listening ...')
 
-BOT_TOKEN = str(os.environ.get('BOT_TOKEN',3))
-
-def handle(msg):
-        chat_id = msg['chat']['id']
-        command = msg['text']
-        user = str(chat_id)
-        msg = ""
-        print( 'User: ' + user + ' Nachricht: ' + str(command) )
-
-        elif command == '/time':
-                bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
-bot = telepot.Bot(BOT_TOKEN)
-bot.message_loop(handle)
-
-print( 'I am listening ...')
-
-while 1:
-    time.sleep(10)
+while true:
+    #time.sleep(10)
+    print("Working")
