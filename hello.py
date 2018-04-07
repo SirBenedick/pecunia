58b447f3ababb96075c964c8347fcf09299143d6
SirBenedick <beneweb@outlook.de>
2018-04-07 13:27:50 +0200

    basic

--- hello.py
+++ hello.py
@@ -26,6 +26,6 @@ def hello_world():
 # def hello_example():
 #     return example
 
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+# @app.route('/hello/<name>')
+# def hello_name(name):
+#    return 'Hello %s!' % name
